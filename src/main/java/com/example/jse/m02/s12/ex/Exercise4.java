3c3915e767013b829024839229fa4996f37a51ad
daedevils <daedevils@daedevils-ZenBook-UX433FN-UX433FN>
2022-02-01 17:33:00 +0100

    **

--- src/main/java/com/example/jse/m02/s12/ex/Exercise4.java
+++ src/main/java/com/example/jse/m02/s12/ex/Exercise4.java
@@ -3,7 +3,7 @@ package com.example.jse.m02.s12.ex;
 public class Exercise4 {
     public static void main(String[] args) {
         /*
-         * TODO: print up to 3 args
+         *
          * 
          * ["ciao", "hello"] -> "ciao" "hello"
          * 
