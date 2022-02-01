89408e272a1ff8ee64538a55c47c3e5718b556e1
daedevils <daedevils@daedevils-ZenBook-UX433FN-UX433FN>
2022-02-01 14:55:58 +0100

    Exercices Done

--- src/main/java/com/example/jse/m01/s09/ex/Two.java
+++ src/main/java/com/example/jse/m01/s09/ex/Two.java
@@ -1,5 +1,7 @@
 package com.example.jse.m01.s09.ex;
 
+import java.util.Arrays;
+
 public class Two {
     public static void main(String[] args) {
         String[][] two = { { "ciao", "buongiorno" }, { "hello", "good morning" } };
@@ -9,6 +11,12 @@ public class Two {
 
         // TODO:
         // (1) Convert the full array in a string and print it
+        System.out.print("Full array is: ");
+        
+        System.out.println(Arrays.toString(two[0])+Arrays.toString(two[1]));
+        
         // (2) Convert just the Italian greetings in a string and print it
+        System.out.print("Italian greeting is: ");
+        System.out.println(Arrays.toString(two[0]));
     }
 }
