5556deec3a35073566dd423bbcef6ef29e91371d
daedevils <daedevils@daedevils-ZenBook-UX433FN-UX433FN>
2022-02-02 09:34:22 +0100

    Cool output

--- src/main/java/com/example/jse/m02/s12/ex/Exercise4.java
+++ src/main/java/com/example/jse/m02/s12/ex/Exercise4.java
@@ -1,5 +1,7 @@
 package com.example.jse.m02.s12.ex;
 
+//import java.util.Arrays;
+
 public class Exercise4 {
     public static void main(String[] args) {
         /*
@@ -12,16 +14,33 @@ public class Exercise4 {
     	//System.out.println(args);
     	//int argLen =args.length;
     	//System.out.print(argLen);
+    	System.out.print("-------------------------------------------\n");
+    	System.out.print("|                                         |\n");
+    	System.out.print("| This is args: ");
+    	
+    	for(String item : args) {
+			System.out.print(item + " ");
+    	}
+    	System.out.print("      |\n");
+    	System.out.print("|                                         |\n");
+    	System.out.print("|                                         |\n");
+    	System.out.print("| This is the requested output:           |\n");
+    	System.out.print("|                                         |\n");
     	int i = 0;
     	for(String item : args) {
     		if(i < 3) {
-    			System.out.print(" - ");
-    			System.out.print(item + "\n");
+    			System.out.print("| - ");
+    			if(i !=1) {
+    				System.out.print(item + "                                  |\n");
+    			} else {
+    				System.out.print(item + "                                    |\n");
+    			}
         	    
     		} else {
     			break;
     		}
     		i++;
     	}
+    	System.out.print("-------------------------------------------\n");
     }
 }
